3da8e56fa29420fe9e4afaa2e8f68db8544e2a57
unknown <your-email@example.com>
2025-08-06 22:31:45 +0530

    Updated layout for better responsive view

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,28 @@
+// pages/Home.jsx
+import About from '../Pages/About.jsx';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Services from './Services';
+import Retail from './Retail';
+import FoodAndBeverage from './FoodAndBeverage';
+import JointVenture from './JointVenture';
+import News from './News';
+import Contact from './Contact';
+
+const Home = () => {
+  return (
+    <>
+      <About />
+      <Stats />
+      <Testimonials />
+      <Services />
+      <Retail />
+      <FoodAndBeverage />
+      <JointVenture />
+      <News />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';       // 👈 Make sure App.jsx is correctly imported
-import './index.css';          // ✅ Global CSS import
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />                  
+      <App />
   </React.StrictMode>
 );
